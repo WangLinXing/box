1efd76f0972ce7d29a4da05d12953571efbf0dde
wangyongqiang <4699633234@qq.com>
2020-09-09 23:02:24 +0800

    第一次提交代码，StringBoot测试代码

---
+++ src/main/java/com/example/test/controller/LoginController.java
@@ -0,0 +1,49 @@
+package com.example.test.controller;
+
+import com.example.test.bean.UserBean;
+import com.example.test.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Controller
+public class LoginController {
+
+    //将Service注入Web层
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/login")
+    public String show(){
+        return "login";
+    }
+
+    @RequestMapping(value = "/loginIn",method = RequestMethod.POST)
+    public ModelAndView login(String name, String password){
+        UserBean userBean = userService.loginIn(name,password);
+        ModelAndView mav = null;
+        if(userBean!=null){
+            //Map<String,String> data = new HashMap<String,String>();
+            String token = UUID.randomUUID().toString().toLowerCase();
+            //data.put("token",token);
+            mav = new ModelAndView("success");
+            mav.addObject("time", new Date());
+            mav.getModel().put("token",token);
+            mav.getModel().put("name", "caoyc");
+            return mav;
+        }else {
+            mav = new ModelAndView("error");
+            return mav;
+        }
+    }
+
+
+
+}
