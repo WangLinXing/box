fee894c02ca4a089436c8f510c3c1fca66bd34df
wangyongqiang <4699633234@qq.com>
2020-09-10 17:13:23 +0800

    修改了HelloController的注释

--- src/main/java/com/example/test/controller/HelloController.java
+++ src/main/java/com/example/test/controller/HelloController.java
@@ -4,7 +4,7 @@ package com.example.test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/* 首页 */
+/* 这里是首页 */
 @Controller
 public class HelloController {
     @RequestMapping("/index")
