0d9eba1503f5ef3ffcdb9794ec58e8de34a92b1e
wangyongqiang <4699633234@qq.com>
2020-09-10 16:58:54 +0800

    HelloController.java 增加了注释

--- src/main/java/com/example/test/controller/HelloController.java
+++ src/main/java/com/example/test/controller/HelloController.java
@@ -4,6 +4,7 @@ package com.example.test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/* 首页 */
 @Controller
 public class HelloController {
     @RequestMapping("/index")
